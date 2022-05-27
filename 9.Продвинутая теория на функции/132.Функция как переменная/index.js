d3aeeb629f524e386b004bd868160eaf06d0fb10
Oleg Bidikhov <olbid99@gmail.com>
2022-05-27 11:29:57 +0300

    доп. функции

---
+++ 9.Продвинутая теория на функции/132.Функция как переменная/index.js
@@ -0,0 +1,35 @@
+// function func() {
+//   return 'Hello';
+// }
+
+// alert(func());
+// alert(func);
+// func = 123;
+
+
+
+// function func1() {
+//   return 3;
+// }
+// let func2 = func1;
+// alert(func1() + func2())
+
+// let func1 = function () {
+//   return 1;
+// }
+// let func2 = function () {
+//   return 2;
+// }
+
+// console.log(func1() + func2());
+
+
+// function func() {
+// 	return '!';
+// }
+
+// func = 'string';
+// alert(func());
+
+
+//Фунции это глаголы, переенные это существительные
