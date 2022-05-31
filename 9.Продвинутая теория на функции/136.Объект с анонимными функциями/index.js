8f24e952b8f5087c6c1985213aec38469b9e2dd2
Oleg Bidikhov <olbid99@gmail.com>
2022-05-31 23:39:34 +0300

    little progress

--- 9.Продвинутая теория на функции/136.Объект с анонимными функциями/index.js
+++ 9.Продвинутая теория на функции/136.Объект с анонимными функциями/index.js
@@ -7,5 +7,9 @@ let obj = {
 console.log(obj.func1() + obj.func2() + obj.func3());
 
 for (let func in obj){
-  console.log(func.obj);
+  console.log(func);
 }
+let math = {
+	square: function(num) {return num**2}, 
+	cube: function(num) {return num**3}, 
+};
